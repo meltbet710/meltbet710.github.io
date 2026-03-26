5f37abca5701ab52c1c7e755788491be4bc1e5f9
meltbet710 <gomesmelt666@gmail.com>
2026-03-26 18:52:49 -0300

    Add files via upload

---
+++ MeltBet/b0101bet.com/assets/theme-0/index.DQUk4nl5.js
@@ -0,0 +1,3 @@
+
+        window.__MANIFEST_ASSETS = 'assets/theme-0/manifest.418c8b29.json';
+        import"./commonChunk.Z6IGE-hq.js";import"./vendorChunk.aicD2K57.js";import"../vendors/vendor-stable.ChvMh5fV.js";const r=Object.freeze(Object.defineProperty({__proto__:null},Symbol.toStringTag,{value:"Module"}));export{r as t};
